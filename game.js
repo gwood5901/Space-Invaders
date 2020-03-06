f8d2d30322936058519b8f6f1fdb8a282b48c201
Gabriel Wood <61399984+gwood5901@users.noreply.github.com>
2020-03-06 10:48:34 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,3 +3,16 @@ var context = canvas.getContext("2d");
 
 context.fillStyle = "gray";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+var canvas = document.getElementById("game-layer");
+var context = canvas.getContext("2d");
+
+context.fillStyle = "gray";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = "red";
+context.fillRect(5, 5, 10, 15);
+context.fillStyle = "blue";
+context.fillRect(25, 25, 20, 20);
+context.fillStyle = "green";
+context.fillRect(50, 50, 20, 40);
